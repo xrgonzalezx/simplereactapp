ed141b912a740200a1b18e7c0600c6c153094815
Rudy Gonzalez <xrgonzalezx@gmail.com>
2017-08-23 16:13:14 -0400

    first version

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <div className="title">
+         <div className="Earthquakes!">
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        </div>
+       </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/EarthquakeList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import earthquakes from './data/earthquakes.js';
+
+class EarthquakeList extends Component {
+  render() {
+    return ()
+  }
+}
+
+export default EarthquakeList;
